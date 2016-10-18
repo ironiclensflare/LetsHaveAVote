2abea15d9d96ae1adfc34cf5d1847295e1526b2a
Stuart Carruthers <stu@ironiclensflare.com>
2016-10-18 21:27:24 +0100

    Added sample SpecFlow tests

---
+++ LetsHaveAVote.Tests/Poll.feature
@@ -0,0 +1,22 @@
+﻿Feature: Poll
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I have then entered 70 into the calculator
+	When I press add
+	Then the result should be 120 on the screen
+
+Scenario: Add two more numbers
+	Given I have entered 90 into the calculator
+	And I have then entered 80 into the calculator
+	When I press add
+	Then the result should be 170 on the screen
+
+Scenario: Add another two numbers
+	Given I have entered 1 into the calculator
+	And I have then entered 8 into the calculator
+	When I press add
+	Then the result should be 9 on the screen
---
+++ LetsHaveAVote.Tests/PollSteps.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace LetsHaveAVote.Tests
+{
+    [Binding]
+    public class PollSteps
+    {
+        private int _number1, _number2, _total;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            _number1 = p0;
+        }
+        
+        [Given(@"I have then entered (.*) into the calculator")]
+        public void GivenIHaveThenEnteredIntoTheCalculator(int p0)
+        {
+            _number2 = p0;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _total = _number1 + _number2;
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(_number1 + _number2, p0);
+        }
+    }
+}
