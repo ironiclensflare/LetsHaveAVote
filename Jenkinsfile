f99b846eaccae5350fa158eeed4d2e3f03101cc1
Stuart Carruthers <stu@ironiclensflare.com>
2016-10-18 19:48:51 +0100

    Added NUnit test runner

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,10 +1,13 @@
 node {
-	stage('Get source from git')
+    stage('Get source from git')
     git 'https://github.com/ironiclensflare/LetsHaveAVote.git'
-	
-	stage('Restore NuGet packages')
-	bat 'C:\\Users\\Stuart\\Desktop\\nuget.exe restore'
-	
-	stage('Build')
-	bat '"C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"'
+    
+    stage('Restore NuGet packages')
+    bat 'C:\\Users\\Stuart\\Desktop\\nuget.exe restore'
+    
+    stage('Build')
+    bat '"C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"'
+    
+    stage('Run unit tests')
+    bat '.\\packages\\NUnit.ConsoleRunner.3.5.0\\tools\\nunit3-console.exe .\\LetsHaveAVote.sln'
 }
--- LetsHaveAVote.Tests/Controllers/HomeControllerTest.cs
+++ LetsHaveAVote.Tests/Controllers/HomeControllerTest.cs
@@ -1,48 +1,39 @@
 ﻿using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LetsHaveAVote.Controllers;
+using NUnit.Framework;
 
 namespace LetsHaveAVote.Tests.Controllers
 {
-    [TestClass]
+    [TestFixture]
     public class HomeControllerTest
     {
-        [TestMethod]
+        [Test]
         public void Index()
         {
-            // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
+            var result = controller.Index() as ViewResult;
 
-            // Assert
             Assert.IsNotNull(result);
         }
 
-        [TestMethod]
+        [Test]
         public void About()
         {
-            // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
-            // Act
-            ViewResult result = controller.About() as ViewResult;
+            var result = controller.About() as ViewResult;
 
-            // Assert
             Assert.AreEqual("Your application description page.", result.ViewBag.Message);
         }
 
-        [TestMethod]
+        [Test]
         public void Contact()
         {
-            // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
+            var result = controller.Contact() as ViewResult;
 
-            // Assert
             Assert.IsNotNull(result);
         }
     }
