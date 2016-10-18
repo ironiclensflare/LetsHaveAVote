15d47189f4cb4f5374dd7698c41b8f092e0770b2
Stuart Carruthers <stu@ironiclensflare.com>
2016-10-18 17:11:06 +0100

    Add project files.

---
+++ LetsHaveAVote/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LetsHaveAVote.MvcApplication" Language="C#" %>
---
+++ LetsHaveAVote/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LetsHaveAVote.Startup))]
+namespace LetsHaveAVote
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
